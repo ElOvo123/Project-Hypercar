15493089b24416ffa1cfde24b229eb0eb28e28d6
DiogoPi <pica.diogomiguel@gmail.com>
2024-08-09 17:51:38 +0100

    fix(main): main file update for general use

--- software/lib/lib.ino
+++ software/lib/lib.ino
@@ -1,23 +1,15 @@
 #include <Arduino.h>
-#include "src/debounce_button/debounce_button.h"
 #include "src/blinking_led/blinking_led.h"
 
 BlinkingLed led1(LED_BUILTIN, LOW, 0, 1000);
 BlinkingLed led2(13, LOW, 0, 500);
-DebouncingButton button1(2, LOW, LOW, 50, 0);
-DebouncingButton button2(1, LOW, LOW, 50, 0);
 
 void setup() {
     Serial.begin(9600);
     led1.InitBlinkLed();
-    led2.InitBlinkLed();
-    button1.InitDebounceButton();
-    button2.InitDebounceButton();
 }
 
 void loop() {
     led1.BlinkLed();
-    led2.BlinkLed();
-    bool button1State = button1.Debounce();
-    bool button2State = button2.Debounce();
+    Serial.print("Welcome to the Hypercar Project");
 }
