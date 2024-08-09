12b080516d0d31aa33f6b0bd3b8ee0e98dc5e1df
DiogoPi <pica.diogomiguel@gmail.com>
2024-08-09 18:25:55 +0100

    fix(main): scheduler functions spacing

--- software/lib/src/task_scheduler/task_scheduler.cpp
+++ software/lib/src/task_scheduler/task_scheduler.cpp
@@ -2,29 +2,29 @@
 #include <Arduino.h>
 
 TaskScheduler::TaskScheduler(int maxTasks) : maxTasks(maxTasks), numTasks(0) {
-    tasks = new Task[maxTasks];
+  tasks = new Task[maxTasks];
 }
 
 TaskScheduler::~TaskScheduler() {
-    delete[] tasks;
+  delete[] tasks;
 }
 
 void TaskScheduler::AddTask(TaskFunction function, unsigned long interval) {
-    if (numTasks < maxTasks) {
-        tasks[numTasks].function = function;
-        tasks[numTasks].interval = interval;
-        tasks[numTasks].lastRun = 0;
-        numTasks++;
-    }
+  if (numTasks < maxTasks) {
+    tasks[numTasks].function = function;
+    tasks[numTasks].interval = interval;
+    tasks[numTasks].lastRun = 0;
+    numTasks++;
+  }
 }
 
 void TaskScheduler::Run() {
-    unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
 
-    for (int i = 0; i < numTasks; i++) {
-        if (currentMillis - tasks[i].lastRun >= tasks[i].interval) {
-            tasks[i].function();
-            tasks[i].lastRun = currentMillis;
-        }
+  for (int i = 0; i < numTasks; i++) {
+    if (currentMillis - tasks[i].lastRun >= tasks[i].interval) {
+      tasks[i].function();
+      tasks[i].lastRun = currentMillis;
     }
+  }
 }
